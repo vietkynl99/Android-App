c17ff4922c48acfe0c83e6287cc85695556047d9
vietkynl99 <107388318+vietkynl99@users.noreply.github.com>
2023-04-20 15:29:48 +0700

    Change fragment name

--- app/src/main/java/com/kynl/myassistant/fragment/FragmentRoom1.java
+++ app/src/main/java/com/kynl/myassistant/fragment/FragmentRoom1.java
@@ -73,7 +73,7 @@ public class FragmentRoom1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_light, container, false);
+        View view = inflater.inflate(R.layout.fragment_room1, container, false);
 
         // gridView
         GridViewAdapter gridViewAdapter = new GridViewAdapter(getContext(), deviceList);
--- app/src/main/java/com/kynl/myassistant/fragment/FragmentRoom4.java
+++ app/src/main/java/com/kynl/myassistant/fragment/FragmentRoom4.java
@@ -61,6 +61,6 @@ public class FragmentRoom4 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fan, container, false);
+        return inflater.inflate(R.layout.fragment_room4, container, false);
     }
 }
