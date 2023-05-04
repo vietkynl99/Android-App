4f2f7dc9b00faca9209da3d47d825cdff0a45ea5
vietkynl99 <vietdohoang1999@gmail.com>
2023-05-04 21:14:32 +0700

    Remove unnecessary function in SuggestionDataAdapter

--- app/src/main/java/com/kynl/myassistant/adapter/SuggestionDataAdapter.java
+++ app/src/main/java/com/kynl/myassistant/adapter/SuggestionDataAdapter.java
@@ -3,23 +3,17 @@ package com.kynl.myassistant.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kynl.myassistant.R;
-import com.kynl.myassistant.model.MessageData;
 
-import java.util.Date;
 import java.util.List;
 
 public class SuggestionDataAdapter extends RecyclerView.Adapter<SuggestionDataAdapter.MessageDataViewHolder> {
-    private final String TAG = "SuggestionDataAdapter";
-    private List<String> suggestionDataList;
+    private final List<String> suggestionDataList;
     private OnSubItemClickListener onSubItemClickListener;
 
     public SuggestionDataAdapter(List<String> suggestionDataList) {
@@ -30,7 +24,7 @@ public class SuggestionDataAdapter extends RecyclerView.Adapter<SuggestionDataAd
     @Override
     public MessageDataViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.suggestion_recycler_view_item, parent, false);
-        return new MessageDataViewHolder(view, onSubItemClickListener);
+        return new MessageDataViewHolder(view);
     }
 
     @Override
@@ -38,12 +32,7 @@ public class SuggestionDataAdapter extends RecyclerView.Adapter<SuggestionDataAd
         holder.suggestionText.setText(suggestionDataList.get(position));
 
         if (onSubItemClickListener != null) {
-            holder.suggestionText.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onSubItemClickListener.onSubItemClick(position, suggestionDataList.get(position));
-                }
-            });
+            holder.suggestionText.setOnClickListener(v -> onSubItemClickListener.onSubItemClick(position, suggestionDataList.get(position)));
         }
     }
 
@@ -56,19 +45,11 @@ public class SuggestionDataAdapter extends RecyclerView.Adapter<SuggestionDataAd
         this.onSubItemClickListener = onSubItemClickListener;
     }
 
-    public void updateItemInserted() {
-        if (suggestionDataList.size() > 0) {
-            notifyItemInserted(suggestionDataList.size() - 1);
-        }
-    }
+    static class MessageDataViewHolder extends RecyclerView.ViewHolder {
+        private final TextView suggestionText;
 
-    class MessageDataViewHolder extends RecyclerView.ViewHolder {
-        private OnSubItemClickListener onSubItemClickListener;
-        private TextView suggestionText;
-
-        public MessageDataViewHolder(@NonNull View itemView, OnSubItemClickListener onSubItemClickListener) {
+        public MessageDataViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.onSubItemClickListener = onSubItemClickListener;
             suggestionText = itemView.findViewById(R.id.suggestionText);
         }
     }
