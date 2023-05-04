edc02ba00ab17fee9e086d68a48318777a199571
vietkynl99 <107388318+vietkynl99@users.noreply.github.com>
2023-05-04 18:01:47 +0700

    Use lambda in FragmentRoom1

--- app/src/main/java/com/kynl/myassistant/fragment/FragmentRoom1.java
+++ app/src/main/java/com/kynl/myassistant/fragment/FragmentRoom1.java
@@ -1,24 +1,21 @@
 package com.kynl.myassistant.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
 
 import com.kynl.myassistant.R;
 import com.kynl.myassistant.adapter.DeviceRecyclerViewAdapter;
-import com.kynl.myassistant.adapter.MenuRecyclerViewAdapter;
-import com.kynl.myassistant.adapter.OnSubItemClickListener;
 import com.kynl.myassistant.model.Device;
 import com.larswerkman.holocolorpicker.OpacityBar;
 
@@ -30,49 +27,16 @@ import static com.kynl.myassistant.common.CommonUtils.SOCKET_REQ_UPDATE_DEVICE;
 
 
 public class FragmentRoom1 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private final String TAG = "FragmentRoom1";
+    private final String TAG = getClass().getName();
     private List<Device> deviceList;
-
 
     public FragmentRoom1() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LightFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentRoom1 newInstance(String param1, String param2) {
-        FragmentRoom1 fragment = new FragmentRoom1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         // list
         deviceList = new ArrayList<>();
@@ -88,22 +52,25 @@ public class FragmentRoom1 extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_room1, container, false);
 
+        Context context = getContext();
+        if (context == null) {
+            Log.e(TAG, "onCreateView: getContext null");
+            return view;
+        }
+
         // lightOpacityBar
         OpacityBar lightOpacityBar = view.findViewById(R.id.lightOpacityBar);
         lightOpacityBar.setColor(ContextCompat.getColor(getContext(), R.color.white));
 
         // deviceRecyclerViewAdapter
         DeviceRecyclerViewAdapter deviceRecyclerViewAdapter = new DeviceRecyclerViewAdapter(deviceList);
-        deviceRecyclerViewAdapter.setOnSubItemClickListener(new OnSubItemClickListener() {
-            @Override
-            public void onSubItemClick(int position, String text) {
-                Intent intent = new Intent(BROADCAST_ACTION);
-                intent.putExtra("event", SOCKET_REQ_UPDATE_DEVICE);
-                intent.putExtra("name", deviceList.get(position).getName());
-                intent.putExtra("type", "switch");
-                intent.putExtra("status", text == "1" ? 1 : 0);
-                LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
-            }
+        deviceRecyclerViewAdapter.setOnSubItemClickListener((position, text) -> {
+            Intent intent = new Intent(BROADCAST_ACTION);
+            intent.putExtra("event", SOCKET_REQ_UPDATE_DEVICE);
+            intent.putExtra("name", deviceList.get(position).getName());
+            intent.putExtra("type", "switch");
+            intent.putExtra("status", text.equals("1") ? 1 : 0);
+            LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
         });
         RecyclerView deviceRecyclerView = view.findViewById(R.id.deviceRecyclerView);
         deviceRecyclerView.setAdapter(deviceRecyclerViewAdapter);
